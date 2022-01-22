18ab36826d4d72717ab8a0597474cbe2c0a51d9b
Ngoc Phuong Anh Nguyen <36784538+AnnadLavender@users.noreply.github.com>
2022-01-22 16:09:11 -0400

    Asignment 1

---
+++ MySecondProgram.java
@@ -0,0 +1,17 @@
+/**
+ @author Ngoc Phuong Anh Nguyen 2712361
+**/
+public class MySecondProgram
+{
+  public static void main(String[] args)
+  {
+    System.out.println("********      *");
+    System.out.println("*      *     * *");
+    System.out.println("*      *    *   *");
+    System.out.println("********   *     *");
+    System.out.println("*         *********");
+    System.out.println("*        *         *");
+    System.out.println("*       *           *");
+    System.out.println("*      *             *");
+  }
+} //MySecondProgram
